9778e7b848dcd0b953d878c1f4394cfe27b33738
Hipolito Coronel <hipolito.coronel@telco.com.ar>
2024-01-06 13:43:01 -0300

    add config mysql db

--- database/mysql.go
+++ database/mysql.go
@@ -1,12 +1,22 @@
 package database
 
 import (
+	"fmt"
+	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func SetupDB() (*gorm.DB, error) {
-	dsn := "root:@tcp(127.0.0.1:3306)/go_chat"
+	// database config
+	DB_USER := os.Getenv("DB_USER")
+	DB_PASSWORD := os.Getenv("DB_PASSWORD")
+	DB_HOST := os.Getenv("DB_HOST")
+	DB_PORT := os.Getenv("DB_PORT")
+	DB_NAME := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
