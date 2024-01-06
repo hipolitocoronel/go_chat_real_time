f201be444d933549c3e00666f7d7e3cd452c8a22
Hipolito Coronel <hipolito.coronel@telco.com.ar>
2024-01-06 14:09:16 -0300

    add gitignore

--- back/database/mysql.go
+++ back/database/mysql.go
@@ -8,16 +8,21 @@ import (
 	"gorm.io/gorm"
 )
 
-func SetupDB() (*gorm.DB, error) {
-	// database config
+// se encarga de realizar la conexión a la base de datos
+func InitMySQL() (*gorm.DB, error) {
+	// 1. obtener variables de entorno
 	DB_USER := os.Getenv("DB_USER")
 	DB_PASSWORD := os.Getenv("DB_PASSWORD")
 	DB_HOST := os.Getenv("DB_HOST")
 	DB_PORT := os.Getenv("DB_PORT")
 	DB_NAME := os.Getenv("DB_NAME")
 
+	// 2. configurar dsn
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
+
+	// 3. realizar conexión
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
 	if err != nil {
 		return nil, err
 	}
